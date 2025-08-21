7f9fa0c1f966f9d2f09e6021c2106b2d10d53ee3
gaurichaulkar12 <gauri.c@networthtracker.in>
2025-08-21 21:11:40 +0530

    Updated Products Name

--- src/assets/all_product.js
+++ src/assets/all_product.js
@@ -37,43 +37,43 @@ import p35_img from '../assets/kids8.jpg'
 let all_product = [
     {
         id: 1,
-        name: "Kurti",
+        name: "Saree",
         category: "women",
         image: p1_img,
         new_price: 50.0,
         old_price: 80.5
     },
 
-    { id: 2, name: "Jeans", category: "men", image:p2_img, new_price: 60.0, old_price: 90.5 },
-    { id: 3, name: "Saree", category: "women", image:p3_img, new_price: 75.0, old_price: 120.0 },
-    { id: 4, name: "Jacket", category: "men", image: p4_img, new_price: 90.0, old_price: 140.0 },
-    { id: 5, name: "T-Shirt", category: "men", image: p5_img, new_price: 45.0, old_price: 70.0 },
-    { id: 6, name: "Dress", category: "women", image: p6_img, new_price: 85.0, old_price: 130.0 },
-    { id: 7, name: "Hoodie", category: "men", image: p7_img, new_price: 95.0, old_price: 150.0 },
-    { id: 8, name: "Leggings", category: "women", image: p8_img, new_price: 55.0, old_price: 100.0 },
-    { id: 9, name: "Shirt", category: "men", image: p9_img, new_price: 70.0, old_price: 110.0 },
-    { id: 10, name: "Jumpsuit", category: "women", image: p10_img, new_price: 88.0, old_price: 135.0 },
-    { id: 11, name: "Sweater", category: "men", image: p11_img, new_price: 92.0, old_price: 145.0 },
-    { id: 12, name: "Gown", category: "women", image: p12_img, new_price: 99.0, old_price: 160.0 },
-    { id: 13, name: "Shorts", category: "men", image: p13_img, new_price: 40.0, old_price: 65.0 },
-    { id: 14, name: "Top", category: "women", image: p14_img, new_price: 52.0, old_price: 85.0 },
-    { id: 15, name: "Kurta", category: "men", image: p15_img, new_price: 78.0, old_price: 115.0 },
-    { id: 16, name: "Skirt", category: "women", image:p16_img, new_price: 73.0, old_price: 110.0 },
+    { id: 2, name: "Brown Coat", category: "men", image:p2_img, new_price: 60.0, old_price: 90.5 },
+    { id: 3, name: "Lehenga Choli", category: "women", image:p3_img, new_price: 75.0, old_price: 120.0 },
+    { id: 4, name: "SweatShirt", category: "men", image: p4_img, new_price: 90.0, old_price: 140.0 },
+    { id: 5, name: "Cargo", category: "men", image: p5_img, new_price: 45.0, old_price: 70.0 },
+    { id: 6, name: "Party Wear", category: "women", image: p6_img, new_price: 85.0, old_price: 130.0 },
+    { id: 7, name: "Pant", category: "men", image: p7_img, new_price: 95.0, old_price: 150.0 },
+    { id: 8, name: "Ghagra", category: "women", image: p8_img, new_price: 55.0, old_price: 100.0 },
+    { id: 9, name: "Kurta", category: "men", image: p9_img, new_price: 70.0, old_price: 110.0 },
+    { id: 10, name: "Dhoti Top", category: "women", image: p10_img, new_price: 88.0, old_price: 135.0 },
+    { id: 11, name: "Ethnic Wear", category: "men", image: p11_img, new_price: 92.0, old_price: 145.0 },
+    { id: 12, name: "Dress", category: "women", image: p12_img, new_price: 99.0, old_price: 160.0 },
+    { id: 13, name: "Jacket", category: "men", image: p13_img, new_price: 40.0, old_price: 65.0 },
+    { id: 14, name: " Cotton Anarkali", category: "women", image: p14_img, new_price: 52.0, old_price: 85.0 },
+    { id: 15, name: "casual Wear", category: "men", image: p15_img, new_price: 78.0, old_price: 115.0 },
+    { id: 16, name: "Pant", category: "women", image:p16_img, new_price: 73.0, old_price: 110.0 },
 
-    { id: 21, name: "Denim Shirt", category: "kid", image: p21_img, new_price: 85.0, old_price: 140.0 },
+    { id: 21, name: "Cotton Dress", category: "kid", image: p21_img, new_price: 85.0, old_price: 140.0 },
    
-    { id: 23, name: "Cargo Pants", category: "kid", image:p23_img , new_price: 88.0, old_price: 130.0 },
+    { id: 23, name: "Dress", category: "kid", image:p23_img , new_price: 88.0, old_price: 130.0 },
     
-    { id: 25, name: "Trousers", category: "kid", image:p25_img , new_price: 76.0, old_price: 120.0 },
-    { id: 27, name: "Tank Top", category: "kid", image: p27_img, new_price: 49.0, old_price: 80.0 },
+    { id: 25, name: "Short Dress", category: "kid", image:p25_img , new_price: 76.0, old_price: 120.0 },
+    { id: 27, name: "Night Suit", category: "kid", image: p27_img, new_price: 49.0, old_price: 80.0 },
    
-    { id: 29, name: "Formal Pants", category: "kid", image:p29_img, new_price: 98.0, old_price: 160.0 },
+    { id: 29, name: "Shorts", category: "kid", image:p29_img, new_price: 98.0, old_price: 160.0 },
 
-    { id: 31, name: "Leather Jacket", category: "kid", image: p31_img, new_price: 150.0, old_price: 250.0 },
+    { id: 31, name: "Kurta Pajama", category: "kid", image: p31_img, new_price: 150.0, old_price: 250.0 },
 
-    { id: 33, name: "Gym Shorts", category: "kid", image: p33_img, new_price: 55.0, old_price: 90.0 },
+    { id: 33, name: "Barbie Dress", category: "kid", image: p33_img, new_price: 55.0, old_price: 90.0 },
   
-    { id:35, name: "Casual Blazer", category: "kid", image: p35_img, new_price: 105.0, old_price: 170.0 },
+    { id:35, name: "Shirt", category: "kid", image: p35_img, new_price: 105.0, old_price: 170.0 },
     
 ]
 
